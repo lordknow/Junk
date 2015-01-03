78c07054d194bd09083b9f3e7faedcae182de4d6
lordknow <lordknow@aol.com>
2015-01-02 19:14:35 -0800

    Test
    Test

---
+++ Junk/test1.cpp
@@ -0,0 +1,9 @@
+//
+//  test1.cpp
+//  
+//
+//  Created by Dnyanesh Mathur on 1/2/15.
+//
+//
+
+#include "test1.h"
---
+++ Junk/test1.h
@@ -0,0 +1,14 @@
+//
+//  test1.h
+//  
+//
+//  Created by Dnyanesh Mathur on 1/2/15.
+//
+//
+
+#ifndef ____test1__
+#define ____test1__
+
+#include <stdio.h>
+
+#endif /* defined(____test1__) */
